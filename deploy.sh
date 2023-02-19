9f3b583bd07dbbf610e17b15f6f97f0979704915
Šarūnas <ssharunas@yahoo.co.uk>
2023-02-19 15:00:04 +0200

    Administravimo mygtukai pastumti į apačią

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 dotnet publish --sc  -r linux-arm
 echo uploading...
-rsync -aurv ./bin/Debug/net6.0/linux-arm/publish/ pi@torontas:/srv/cshtml/kortos/
+rsync -aurv ./bin/Debug/net7.0/linux-arm/publish/ pi@torontas:/srv/cshtml/kortos/
 
